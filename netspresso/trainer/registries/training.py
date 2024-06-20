fd8e9e9fbe0df502f65d984850db6710ff308fde
Byeongman Lee <bmlee@nota.ai>
2024-06-20 15:11:45 +0900

    #253 Add dataclass for trainer config (#254)

--- netspresso/trainer/registries/training.py
+++
@@ -1,7 +0,0 @@
-from netspresso_trainer.cfg import ClassificationScheduleConfig, DetectionScheduleConfig, SegmentationScheduleConfig
-
-TRAINING_CONFIG_TYPE = {
-    "classification": ClassificationScheduleConfig,
-    "detection": DetectionScheduleConfig,
-    "segmentation": SegmentationScheduleConfig,
-}
