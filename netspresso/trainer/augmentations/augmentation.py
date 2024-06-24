c5fb3751947a6e0453092213001251e56e873a12
Only-bottle <bmlee@nota.ai>
2024-06-24 19:53:44 +0900

    Add augmentation(CenterCrop, HSVJitter, Mixing, MosaicDetection, PoseTopDownAffine, RandomErasing, RandomResize)

--- netspresso/trainer/augmentations/augmentation.py
+++ netspresso/trainer/augmentations/augmentation.py
@@ -31,6 +31,12 @@ class AugmentationConfig:
 
 
 @dataclass
+class CenterCrop(Transform):
+    name: str = 'centercrop'
+    size: int = DEFAULT_IMG_SIZE
+
+
+@dataclass
 class ColorJitter(Transform):
     name: str = 'colorjitter'
     brightness: Optional[float] = 0.25
@@ -38,6 +44,38 @@ class ColorJitter(Transform):
     saturation: Optional[float] = 0.25
     hue: Optional[float] = 0.1
     p: Optional[float] = 0.5
+
+
+@dataclass
+class HSVJitter(Transform):
+    name: str = "hsvjitter"
+    h_mag: int = 5
+    s_mag: int = 30
+    v_mag: int = 30
+
+
+@dataclass
+class Mixing(Transform):
+    name: str = "mixing"
+    mixup: Optional[List[float, float]] = None
+    cutmix: Optional[List[float, float]] = None
+    inplace: bool = False
+
+
+@dataclass
+class MosaicDetection(Transform):
+    name: str = "mosaicdetection"
+    size: List = field(default_factory=lambda: [DEFAULT_IMG_SIZE, DEFAULT_IMG_SIZE])
+    mosaic_prob: float = 1.0
+    affine_scale: List[float, float] = [0.5, 1.5]
+    degrees: float = 10.0
+    translate: float = 0.1
+    shear: float = 2.0
+    enable_mixup: bool = True
+    mixup_prob: float = 1.0
+    mixup_scale: List[float, float] = [0.5, 1.5]
+    fill: int = 114
+    mosaic_off_epoch: int = 10
 
 
 @dataclass
@@ -49,9 +87,46 @@ class Pad(Transform):
 
 
 @dataclass
+class PoseTopDownAffine(Transform):
+    name: str = "posetopdownaffine"
+    scale: List[float, float] = [0.75, 1.25]
+    scale_prob: float = 1.
+    translate: float = 0.1
+    translate_prob: float = 1.
+    rotation: int = 60
+    rotation_prob: float = 1.
+    size: List = field(default_factory=lambda: [DEFAULT_IMG_SIZE, DEFAULT_IMG_SIZE])
+
+
+@dataclass
 class RandomCrop(Transform):
     name: str = 'randomcrop'
     size: int = DEFAULT_IMG_SIZE
+
+
+@dataclass
+class RandomErasing(Transform):
+    name: str = "randomerasing"
+    p: float = 0.5
+    scale: List[float, float] = [0.02, 0.33]
+    ratio: List[float, float] = [0.3, 3.3]
+    value: Optional[int] = 0
+    inplace: bool = False
+
+
+@dataclass
+class RandomHorizontalFlip(Transform):
+    name: str = 'randomhorizontalflip'
+    p: float = 0.5
+
+
+@dataclass
+class RandomResize(Transform):
+    name: str = "randomresize"
+    base_size: List = [256, 256]
+    stride: int = 32
+    random_range: int = 4
+    interpolation: str = "bilinear"
 
 
 @dataclass
@@ -61,12 +136,6 @@ class RandomResizedCrop(Transform):
     scale: List = field(default_factory=lambda: [0.08, 1.0])
     ratio: List = field(default_factory=lambda: [0.75, 1.33])
     interpolation: Optional[str] = 'bilinear'
-
-
-@dataclass
-class RandomHorizontalFlip(Transform):
-    name: str = 'randomhorizontalflip'
-    p: float = 0.5
 
 
 @dataclass
