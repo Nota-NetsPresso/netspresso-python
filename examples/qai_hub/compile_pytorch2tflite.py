93ceddbcd8198212df277b61d58bf89e42ad88d1
Only-bottle <bmlee@nota.ai>
2024-07-24 13:59:03 +0900

    Update example code

--- examples/qai_hub/compile_pytorch2tflite.py
+++ examples/qai_hub/compile_pytorch2tflite.py
@@ -5,7 +5,7 @@ import torchvision
 
 from netspresso import QAIHub
 from netspresso.qai_hub import Device
-from netspresso.qai_hub.options import CompileOptions, Runtime
+from netspresso.qai_hub.options import CompileOptions, Runtime, ComputeUnit, ProfileOptions, TfliteOptions
 
 # Using pre-trained MobileNet
 torch_model = torchvision.models.mobilenet_v2(pretrained=True)
@@ -19,26 +19,31 @@ input_shape: Tuple[int, ...] = (1, 3, 64, 64)
 # INPUT_SHAPES = [{"batch": 1, "channel": 3, "dimension": [224, 224]}]
 options = CompileOptions(
     target_runtime=Runtime.TFLITE,
+    compute_unit=[ComputeUnit.CPU, ComputeUnit.GPU]
 )
 
 conversion_task = converter.convert_model(
     input_model_path="./examples/sample_models/pytorch_model_automatic_0.9.onnx",
     output_dir="./outputs/qai_hub/qnn_context_binary",
-    target_device_name=Device(name="Samsung Galaxy S23 (Family)"),
+    target_device_name=Device(name="QCS6490 (Proxy)"),
     options=options,
     input_shape=input_shape,
 )
-
-import ipdb; ipdb.set_trace()
 print(conversion_task)
 
-# benchmarker = qai_hub.benchmarker()
+# import ipdb; ipdb.set_trace()
+benchmarker = qai_hub.benchmarker()
 
-# profile_job = benchmarker.benchmark_model(
-#     model=conversion_task.converted_model_path,
-#     output_dir="./outputs/qai_hub/qnn_context_binary",
-#     device=Device("Samsung Galaxy S23 (Family)"),
-# )
+benchmark_option = ProfileOptions(
+    compute_unit=[ComputeUnit.GPU],
+    tflite_options=TfliteOptions(number_of_threads=4)
+)
+
+profile_job = benchmarker.benchmark_model(
+    input_model_path=conversion_task.converted_model_path,
+    target_device_name=Device("QCS6490 (Proxy)"),
+    options=benchmark_option,
+)
 
 # input_tensor = np.random.random(input_shape).astype(np.float32)
 
