611456348e13bf8bca19069633db20ecd73399ec
Only-bottle <bmlee@nota.ai>
2024-08-05 18:53:58 +0900

    Remove QAI Hub integration

--- netspresso/__init__.py
+++ netspresso/__init__.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 
-from .netspresso import TAO, NetsPresso, QAIHub
+from .netspresso import TAO, NetsPresso
 
-__all__ = ["NetsPresso", "TAO", "QAIHub"]
+__all__ = ["NetsPresso", "TAO"]
 
 
 version = (Path(__file__).parent / "VERSION").read_text().strip()
