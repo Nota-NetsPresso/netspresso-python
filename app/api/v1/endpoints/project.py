db1dc099eab3cea47152b87c0276c26465f8846c
Byeongman Lee <bmlee@nota.ai>
2024-12-04 19:48:14 +0900

    #404 Define the schema for detailed project information (#409)

--- app/api/v1/endpoints/project.py
+++ app/api/v1/endpoints/project.py
@@ -3,7 +3,16 @@ from uuid import uuid4
 
 from fastapi import APIRouter
 
-from app.api.v1.schemas.project import ProjectCreate, ProjectPayload, ProjectResponse, ProjectsResponse
+from app.api.v1.schemas.project import (
+    ExperimentStatus,
+    ModelSummary,
+    ProjectCreate,
+    ProjectDetailPayload,
+    ProjectDetailResponse,
+    ProjectResponse,
+    ProjectsResponse,
+    ProjectSummaryPayload,
+)
 
 router = APIRouter()
 
@@ -14,7 +23,7 @@ def create_project(
     request_body: ProjectCreate,
 ) -> ProjectResponse:
 
-    project = ProjectPayload(
+    project = ProjectSummaryPayload(
         project_id=str(uuid4()),
         project_name=request_body.project_name,
         user_id=str(uuid4()),
@@ -32,7 +41,7 @@ def get_projects(
 ) -> ProjectsResponse:
 
     projects = [
-        ProjectPayload(
+        ProjectSummaryPayload(
             project_id=str(uuid4()),
             project_name="project_test_1",
             user_id=str(uuid4()),
@@ -40,3 +49,36 @@ def get_projects(
     ]
 
     return ProjectsResponse(data=projects)
+
+
+@router.get("/{project_id}", response_model=ProjectDetailResponse)
+def get_project(
+    *,
+    project_id: str
+) -> ProjectDetailResponse:
+
+    models = [
+        ModelSummary(
+            model_id="9beb6f14-fe8a-4d70-8243-c51f5d7f36f8",
+            name="yolox_s_test",
+            type="trained_model",
+            status="in_progress",
+            latest_experiments=ExperimentStatus(convert="not_started", benchmark="not_started")
+        ),
+        ModelSummary(
+            model_id="3aab6fb0-9852-4794-b668-676c06246564",
+            name="yolox_l_test",
+            type="compressed_model",
+            status="completed",
+            latest_experiments=ExperimentStatus(convert="completed", benchmark="completed")
+        )
+    ]
+
+    project = ProjectDetailPayload(
+        project_id=str(uuid4()),
+        project_name="project_test_1",
+        user_id=str(uuid4()),
+        models=models
+    )
+
+    return ProjectDetailResponse(data=project)
--- app/api/v1/schemas/project.py
+++ app/api/v1/schemas/project.py
@@ -3,6 +3,7 @@ from typing import List
 from pydantic import BaseModel, ConfigDict, Field, field_validator
 
 from app.api.v1.schemas.base import ResponseItem, ResponsePaginationItems
+from netspresso.enums import Status
 
 
 class ProjectCreate(BaseModel):
@@ -15,16 +16,37 @@ class ProjectCreate(BaseModel):
         return project_name
 
 
-class ProjectPayload(ProjectCreate):
+class ProjectSummaryPayload(ProjectCreate):
     model_config = ConfigDict(from_attributes=True)
 
     project_id: str = Field(..., description="The unique identifier for the project.")
     user_id: str = Field(..., description="The unique identifier for the user associated with the project.")
 
 
+class ExperimentStatus(BaseModel):
+    convert: Status = Field(Status.NOT_STARTED, description="The status of the conversion experiment.")
+    benchmark: Status = Field(Status.NOT_STARTED, description="The status of the benchmark experiment.")
+
+
+class ModelSummary(BaseModel):
+    model_id: str = Field(..., description="The unique identifier for the model.")
+    name: str = Field(..., description="The name of the model.")
+    type: str = Field(..., description="The type of the model (e.g., trained_model, compressed_model).")
+    status: Status = Field(Status.NOT_STARTED, description="The current status of the model.")
+    latest_experiments: ExperimentStatus = Field(..., description="The latest status of experiments for the model.")
+
+
+class ProjectDetailPayload(ProjectSummaryPayload):
+    models: List[ModelSummary] = Field(..., description="The list of models associated with the project.")
+
+
 class ProjectResponse(ResponseItem):
-    data: ProjectPayload
+    data: ProjectSummaryPayload
+
+
+class ProjectDetailResponse(ResponseItem):
+    data: ProjectDetailPayload
 
 
 class ProjectsResponse(ResponsePaginationItems):
-    data: List[ProjectPayload]
+    data: List[ProjectSummaryPayload]
