8f831178267b6e0218f3212f3e06e50859ca958b
Only-bottle <bmlee@nota.ai>
2024-12-04 17:29:54 +0900

    Add project schema for create

---
+++ app/api/v1/schemas/project.py
@@ -0,0 +1,24 @@
+from pydantic import BaseModel, ConfigDict, Field, field_validator
+
+from app.api.v1.schemas.base import ResponseItem
+
+
+class ProjectCreate(BaseModel):
+    project_name: str = Field(..., description="The name of the project to be created.")
+
+    @field_validator("project_name")
+    def validate_length_of_project_name(cls, project_name: str) -> str:
+        if len(project_name) > 30:
+            raise ValueError("The project_name can't exceed 30 characters.")
+        return project_name
+
+
+class ProjectPayload(ProjectCreate):
+    model_config = ConfigDict(from_attributes=True)
+
+    project_id: str = Field(..., description="The unique identifier for the project.")
+    user_id: str = Field(..., description="The unique identifier for the user associated with the project.")
+
+
+class ProjectResponse(ResponseItem):
+    data: ProjectPayload
