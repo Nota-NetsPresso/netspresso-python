24927c3196598b719994652b7417333e52f26b57
Only-bottle <bmlee@nota.ai>
2024-12-04 17:30:40 +0900

    Add project router initialization and API routing setup

---
+++ app/api/api.py
@@ -0,0 +1,6 @@
+from fastapi import APIRouter
+
+from app.api.v1.endpoints import project
+
+api_router = APIRouter()
+api_router.include_router(project.router, prefix="/projects", tags=["project"])
