1265df3a8e38f0b7c175c8d7e70ed196ffd2e796
Only-bottle <bmlee@nota.ai>
2024-12-20 12:25:25 +0900

    Add get_db_session

--- netspresso/utils/db/models/__init__.py
+++ netspresso/utils/db/models/__init__.py
@@ -1,4 +1,6 @@
+from netspresso.utils.db.models.model import TrainedModel
 from netspresso.utils.db.models.project import Project
+from netspresso.utils.db.models.train import TrainTask
 from netspresso.utils.db.models.user import User
 from netspresso.utils.db.session import Base, engine
 
@@ -6,6 +8,8 @@ Base.metadata.create_all(engine)
 
 
 __all__ = [
+    "TrainedModel",
     "Project",
+    "TrainTask",
     "User",
 ]
